c7d8f4f83b5b89a23255d6083c7106e82131bcf0
maddyhunter76 <bhattasuraj76@gmail.com>
2020-07-20 20:24:59 +0545

    username added in navbar

--- resources/views/front/include/navbar.blade.php
+++ resources/views/front/include/navbar.blade.php
@@ -18,7 +18,7 @@
             <li class="nav-item mx-3">
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                    Logout
+                    {{auth()->user()->email}}, Logout
                 </a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                     @csrf
